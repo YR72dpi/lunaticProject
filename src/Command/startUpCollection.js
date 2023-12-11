ad291edd596458f3992df99dd64fe9c2706a79c2
Rousselle <ylan.rousselle76@gmail.com>
2023-12-11 22:23:43 +0100

    feat: command pour ajouter 100 n° random en database

---
+++ src/Command/startUpCollection.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+import { simpleRandom } from '../util/serverAlgo.js';
+
+const prisma = new PrismaClient()
+
+const x = 100;
+
+/**
+ * Put x random for start the service
+ */
+const startUpCollection = async () => {
+    const randomNumber = await simpleRandom();
+    try {
+        await prisma.number.create({
+            data: {
+              origin: 'SERVER',
+              number: randomNumber,
+            },
+          })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+for (let i = 1; i <= x; i++) { 
+    startUpCollection()
+    console.log("[START UP] : Add a random number on data (" + i + " / " + x + ")")
+}
